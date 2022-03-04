ecf681e214d8a06927b8d5e09d903c44a3d76de4
31847 <90313724+31847@users.noreply.github.com>
2022-03-04 14:40:10 +0000

    Start

--- Adventure Game #1.py
+++ Adventure Game #1.py
@@ -5,5 +5,16 @@ def main():
 
     life_counter = 3
     print (f"You have {life_counter} remaining lives")
+    print ("There are 4 directions you can move. Forward, left, right or backwards")
+    direction = input("Which direction do you want to go? ")
+    if direction == "forward":
+        print("You went forward")
+    elif direction == "backwards":
+        print("You went backwards")
+    elif direction == "left":
+        print("You went left")
+    elif direction == "right":
+        print("you went right")
+    else: print ("Sorry, not recognised!")
 
 main()
---
+++ Main python game.py
@@ -0,0 +1,4 @@
+def main ():
+    print ("Welcome to my game! Temple Explorer")
+    player_name = input("What is your name? ")
+    print ("Hi " + player_name)
